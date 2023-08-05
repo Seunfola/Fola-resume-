adf669ca538a053bf00a657331632461ddb17aad
Folahan <seunfola1@gmail.com>
2023-08-05 08:52:00 +0100

    fixed the send toggle button bug for the contact form and excess margins

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,32 +1,16 @@
 import React, { useState } from 'react';
 import './contact.css';
-import emailjs from '@emailjs/browser';
+import { useForm, ValidationError } from '@formspree/react';
 import { useRef, useContext } from 'react';
 import { themeContext } from "../../Context";
 const Contact = () => {
+    const [state, handleSubmit] = useForm("xzbldqvd");
+    const form = useRef();
     const theme = useContext(themeContext);
     const darkMode = theme.state.darkMode;
-    const form = useRef();
-
-    const [done, setDone] = useState(false)
-
-    const sendEmail = (e) => {
-        e.preventDefault();
-
-        emailjs.sendForm(
-            "service_2mu5xtl",
-            "template_m5udu2c",
-            form.current,
-            "VLwg1ltOWvnCYAiK_"
-        )
-            .then((result) => {
-                console.log(result.text);
-                setDone(true);
-                form.reset();
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
+    if (state.succeeded) {
+        return <p className='message'>Your Message Was Delivered Successfully!</p>;
+    }
 
 
 
@@ -40,12 +24,27 @@ const Contact = () => {
                 </div>
             </div>
             <div className='c-right'>
-                <form ref={form} onSubmit={sendEmail}>
-                    <input type='text' name='user_name' className='user' Placeholder='Name' />
-                    <input type='email' name='user_email' className='user' Placeholder='Email' />
+                <form ref={form} onSubmit={handleSubmit}>
+                    <input type='text' id='name' name='name' className='user' Placeholder='Name' />
+                    <ValidationError
+                        prefix="Name"
+                        field="name"
+                        errors={state.errors}
+                    />
+                    <input type='email' id='email' name='email' className='user' Placeholder='Email' />
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
                     <textarea name='message' className='user' autoComplete='on' spellCheck='true' placeholder='Message' />
-                    <input type='submit' value='Send' className='button' />
-                    <span>{done && "Thanks for Contacting me!"}</span>
+                    <ValidationError
+                        prefix="Message"
+                        field="message"
+                        errors={state.errors}
+                    />
+                    <input type='submit' value='Send' className='button' disabled={state.submitting} />
+                    {/* <span>{done && "Thanks for Contacting me!"}</span> */}
 
                 </form>
             </div>
