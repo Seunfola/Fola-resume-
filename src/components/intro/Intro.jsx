e94e20dfb1fced4703eb9173c243568ff2e53895
Folahan <seunfola1@gmail.com>
2023-08-05 09:02:47 +0100

    added tags to the icons

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,6 +4,7 @@ import Github from "../../img/github.png";
 import LinkedIn from "../../img/linkedin.png";
 import instagram from "../../img/instagram.png";
 import newfoli from "../../img/newfoli.jpg";
+import { Gitub } from "@iconscout/react-unicons/icons/uil-github";
 
 const Intro = () => {
   return (
@@ -16,16 +17,19 @@ const Intro = () => {
           A pragmatic, diligent and talented Frontend Developer with high level of <br />Experience in web designing
           and development, producing quality work.
         </span>
-       
+
         <div className="i-icons">
           <a href="https://github.com/Seunfola">
             <img src={Github} alt="github" />
+            <p className="icon-text">Gitub</p>
           </a>
           <a href="https://www.linkedin.com/in/folahan-m-37b48bb2/">
             <img src={LinkedIn} alt="linkedin" />
+            <p className="icon-text">LinkedIn</p>
           </a>
           <a href="https://www.instagram.com/clanbreed/">
             <img src={instagram} alt="instgram" />
+            <p className="icon-text">Instagram</p>
           </a>
         </div>
       </div>
