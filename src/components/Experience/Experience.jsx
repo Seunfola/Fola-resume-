8c63682e009a143f653a9fa0ddea18e5c2c72cb0
Folahan <seunfola1@gmail.com>
2023-12-03 13:29:26 +0100

    fixed the scroll-bar

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -11,12 +11,10 @@ const Experience = () => {
             </div>
             <div className="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
                 <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasScrollingLabel">Colored with scrolling</h5>
+                    <h5 className="offcanvas-title" id="offcanvasScrollingLabel"></h5>
                     <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-                <div className="offcanvas-body">
-                    <p>Try scrolling the rest of the page to see this option in action.</p>
-                </div>
+
             </div>
             <div className="offcanvas offcanvas-start" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="offcanvasWithBackdropLabel">
                 <div className="offcanvas-header">
